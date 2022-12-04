e15088e249ebaf85a91c7d0ffebb17d2d0e8b2b6
ViniciusLaroza <viniciusmeyerlaroza@gmail.com>
2022-12-04 13:54:35 -0300

    Commit Inicial

---
+++ app/src/main/java/br/edu/ifsp/ads/pdm/aulas/moviesmanager/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.ads.pdm.aulas.moviesmanager.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.ads.pdm.aulas.moviesmanager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
